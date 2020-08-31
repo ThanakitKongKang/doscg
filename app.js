38fc3840e0560784c10b180858aad2600ac6bc1f
ThanakitKongkang <thanakit@kkumail.com>
2020-08-31 22:43:46 +0700

    fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
   let msg = req.body.events[0].message.text
@@ -13,7 +12,7 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200)
 })
 app.listen(port)
-function reply (reply_token) {
+function reply (reply_token, msg) {
   let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {Mz+DV1Z3aSuQ65BJ9O6gW9f/JU524lLrGfrtj/gb5m48KU0VmoQxJ3ZVRw71e+Up1UlZgUxrkRY6KC8unOAAN/tWXDXOz+8U0WefjdLObzr2FzFuXnxwlteTJDxWKfR5zO4xH5VLnkSfbLW5joeGHQdB04t89/1O/w1cDnyilFU=}'
