7b314eddcc05d3f1893a80a07ee69fa10bc69d1a
ThanakitKongkang <thanakit@kkumail.com>
2020-09-01 01:10:26 +0700

    line notify

--- app.js
+++ app.js
@@ -9,31 +9,28 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
   let msg = req.body.events[0].message.text
-  let userId = req.body.events[0].userId
-  reply(reply_token, msg, userId)
+  reply(reply_token, msg)
   res.sendStatus(200)
 })
 app.listen(port)
-function reply (reply_token, msg, userId) {
+function reply (reply_token, msg) {
   let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {Mz+DV1Z3aSuQ65BJ9O6gW9f/JU524lLrGfrtj/gb5m48KU0VmoQxJ3ZVRw71e+Up1UlZgUxrkRY6KC8unOAAN/tWXDXOz+8U0WefjdLObzr2FzFuXnxwlteTJDxWKfR5zO4xH5VLnkSfbLW5joeGHQdB04t89/1O/w1cDnyilFU=}'
   }
   if (msg.toLowerCase() !== 'hey') {
     setTimeout(() => {
+      headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer x6SFl0tcbqc00ixjrWU7dRmrFPQtEZfEBS3NQ6vg4Mm'
+      }
       let body = JSON.stringify({
-        to: 'Ua713b8f663a3db5709fd064dc588139e',
-        messages: [{
-          type: 'text',
-          text: 'Hello' + userId
-        },
-        {
-          type: 'text',
-          text: 'How are you?'
-        }]
+        // to: 'Ua713b8f663a3db5709fd064dc588139e',
+        messages: 'Can\'t answer customer question'
       })
       request.post({
-        url: 'https://api.line.me/v2/bot/message/push',
+        url: 'https://notify-api.line.me/api/notify',
+        // url: 'https://api.line.me/v2/bot/message/push',
         headers: headers,
         body: body
       }, (err, res, body) => {
